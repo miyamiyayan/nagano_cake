1301f6b840f41ee62ed161a4991533a92909463b
miyamiyayan <dmkm.25.active@gmail.com>
2021-10-17 12:05:29 +0000

    [update]ヘッダー作成と管理者とユーザーによる記載内容の変更を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,30 @@
 Rails.application.routes.draw do
+
+  devise_for :admin, controllers: {
+   sessions:      'admin/sessions',
+   passwords:     'admin/passwords',
+   registrations: 'admin/registrations'
+  }
+
+  devise_for :customers, controllers: {
+   sessions:      'public/sessions',
+   passwords:     'public/passwords',
+   registrations: 'public/registrations'
+  }
+
+  root to: 'public/homes#top'
+  get "/about" => 'public/homes#about'
+  get "/items" => 'public/items#index'
+  get "/items/:id" => 'public/items#show'
+  resources :customers, only:[:show, :edit, :update]
+  post "/customers/confirm" => 'public/customers#confirm'
+  patch "/customers/withdraw" => 'public/customers#withdraw'
   
- devise_for :admins, controllers: {
-  sessions:      'admins/sessions',
-  passwords:     'admins/passwords',
-  registrations: 'admins/registrations'
-}
-devise_for :customers, controllers: {
-  sessions:      'public/sessions',
-  passwords:     'public/passwords',
-  registrations: 'public/registrations'
-}
+
+  namespace :admin do
+    get '/' => 'homes#top'
+    
+  end
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
