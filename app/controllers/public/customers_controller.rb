bc0772298da62b1c1cdbe6656b0a98f8e6857660
miyamiyayan <dmkm.25.active@gmail.com>
2021-10-23 10:53:41 +0000

    [update]カスタマー側会員情報編集、NEXT退会処理

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,16 +1,33 @@
 class Public::CustomersController < ApplicationController
-  def show
-  end
 
   def edit
+    @customer = Customer.find(current_customer.id)
+  end
+
+  def show
+    @customer = Customer.find(current_customer.id)
   end
 
   def update
+    customer = Customer.find(current_customer.id)
+    customer.update(customer_params)
+    redirect_to customers_path
   end
 
   def confirm
+    @customer = Customer.find(current_customer.id)
   end
 
   def withdraw
+    customer = Customer.find(current_customer.id)
+    customer.update(customer_params)
+    redirect_to root_path
   end
+
+private
+
+ def customer_params
+   params.require(:customer).permit(:first_name, :last_name, :last_name_kana, :first_name_kana, :email, :postal_code, :address, :telephone_number, :is_active)
+ end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,28 @@
 Rails.application.routes.draw do
 
+# 以下顧客側
+# トップとアバウト
+  root to: 'public/homes#top'
+  get "/about" => 'public/homes#about'
+# 商品一覧と詳細
+  get "/items" => 'public/items#index'
+  get "/items/:id" => 'public/items#show'
+# マイページと編集
+  get "/customers/" => 'public/customers#show'
+  get "/customers/edit" => 'public/customers#edit'
+  patch "/customers/" => 'public/customers#update'
+  get "/customers/confirm" => 'public/customers#confirm'
+  patch "/customers/withdraw" => 'public/customers#withdraw'
+
+
+  namespace :admin do
+    get '/' => 'homes#top'
+    resources :genres, only:[:index, :create, :edit, :update]
+    resources :items, except: [:destroy]
+    resources :customers, except: [:create, :destroy, :new]
+
+  end
+
   devise_for :admin, controllers: {
    sessions:      'admin/sessions',
    passwords:     'admin/passwords',
@@ -11,23 +34,6 @@ Rails.application.routes.draw do
    passwords:     'public/passwords',
    registrations: 'public/registrations'
   }
-
-  root to: 'public/homes#top'
-  get "/about" => 'public/homes#about'
-  get "/items" => 'public/items#index'
-  get "/items/:id" => 'public/items#show'
-  resources :customers, only:[:show, :edit, :update]
-  post "/customers/confirm" => 'public/customers#confirm'
-  patch "/customers/withdraw" => 'public/customers#withdraw'
-
-
-  namespace :admin do
-    get '/' => 'homes#top'
-    resources :genres, only:[:index, :create, :edit, :update]
-    resources :items, except: [:destroy]
-    resources :customers, except: [:create, :destroy, :new]
-
-  end
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
