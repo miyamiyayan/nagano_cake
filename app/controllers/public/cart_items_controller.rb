27e13a6c38fcba8456581d7bbf5d06cde38af30d
miyamiyayan <dmkm.25.active@gmail.com>
2021-10-27 03:53:32 +0000

    [update]カートへ追加※ロールバック中

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -14,6 +14,15 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
+    cart_item = CartItem.new(cart_item_params)
+    cart_item.save
+    redirect_to cart_items_path
   end
 
 end
+
+private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :customer_id, :amount)
+  end
