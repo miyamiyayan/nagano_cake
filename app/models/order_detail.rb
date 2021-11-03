6afbfe9743a397af49f7564edaaf2000a375be4a
miyamiyayan <dmkm.25.active@gmail.com>
2021-11-03 11:53:57 +0000

    [update]注文確認詳細画面完成、NEXTアップデートメソッド定義

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,6 +1,13 @@
 class OrderDetail < ApplicationRecord
 
+
+  def subtotal
+    item.with_tax_price.to_i * amount.to_i
+  end
+
   belongs_to :item
   belongs_to :order
+
+  enum make_status: { huka: 0, mati: 1, seisaku: 2, seisakuok: 3 }
 
 end
