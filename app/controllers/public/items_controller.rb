d143d5e7049f38cd3dc568fd7e371e5acdb49789
miyamiyayan <dmkm.25.active@gmail.com>
2021-11-10 12:28:32 +0000

    [update]商品詳細とカスタマーマイページ

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -5,6 +5,7 @@ class Public::ItemsController < ApplicationController
   end
 
   def show
+    @genres = Genre.all.page(params[:page]).per(4)
     @item = Item.find(params[:id])
     @cart_item = CartItem.new
   end
