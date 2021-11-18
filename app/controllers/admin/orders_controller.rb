94fce6d07a1040b4fc1a37e55b0ab6c7a8541b85
miyamiyayan <dmkm.25.active@gmail.com>
2021-11-18 14:04:42 +0000

    [update]エラー文章の日本語化

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,5 +1,5 @@
 class Admin::OrdersController < ApplicationController
-  
+
   before_action :authenticate_admin!
 
   def show
@@ -11,6 +11,15 @@ class Admin::OrdersController < ApplicationController
   def update
     order = Order.find(params[:id])
     order.update(order_params)
+
+    if
+      order.status == "kakunin"
+      order.order_details.each do |order_detail|
+      order_detail.update(make_status: "mati")
+      end
+    end
+
+
     redirect_to admin_order_path(order.id)
   end
 
