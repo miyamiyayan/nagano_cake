0f69b773e6e9184e4e92d85891d274c5379759d0
miyamiyayan <dmkm.25.active@gmail.com>
2021-11-20 10:39:56 +0000

    [update]注文ステータス連動機能完成

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,12 +1,30 @@
 class Admin::OrderDetailsController < ApplicationController
-  
+
   before_action :authenticate_admin!
 
   def update
-    @order = Order.find(params[:id])
-    order_detail = @order.order_details
+    order_detail = OrderDetail.find(params[:id])
     order_detail.update(order_detail_params)
-    redirect_to admin_order_path(@order.id)
+
+    if
+      order_detail.make_status == "seisaku"
+      order_detail.order.update(status: "sakusei")
+    else
+      sum = 0
+      @order_details = order_detail.order.order_details
+      @order_details.each do |order_detail|
+        if
+          order_detail.make_status == "seisakuok"
+          sum += 1
+        end
+      end
+      if
+        @order_details.count == sum
+        order_detail.order.update(status: "hassoumae")
+      end
+    end
+
+    redirect_to admin_order_path(order_detail.order_id)
   end
 
 end
